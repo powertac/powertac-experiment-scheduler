1693b4165c039b1c7335b960f2f34cd95d0d8fc2
Frederik Milkau <fmilkau@uni-koeln.de>
2021-08-13 09:52:09 +0200

    made allowed origins configurable via properties

--- src/main/java/org/powertac/rachma/api/AllowedOriginsProvider.java
+++ src/main/java/org/powertac/rachma/api/AllowedOriginsProvider.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-// TODO : replace with some sort of service discovery
+// TODO : keep for now; replace later with spring default config
+//        (https://docs.spring.io/spring-boot/docs/current/reference/html/actuator.html#actuator.endpoints.cors)
 
 @Component
 public class AllowedOriginsProvider {
@@ -15,20 +17,19 @@ public class AllowedOriginsProvider {
     @Value("${application.mode}")
     private String applicationMode;
 
+    @Value("#{'${application.api.allowed-origins}'.split(',')}")
+    private List<String> allowedOrigins;
+
     public String[] getAllowedOrigins() {
-
         Set<String> allowedOrigins = new HashSet<>();
-
         if (ExperimentSchedulerApplication.Mode.DEVELOPMENT.equals(ExperimentSchedulerApplication.Mode.from(applicationMode))) {
             allowedOrigins.add("http://localhost:9000");
         }
-
         String serviceManagerUri = System.getenv("SERVICE_MANAGER_URI");
-
         if (null != serviceManagerUri) {
             allowedOrigins.add(serviceManagerUri);
         }
-
+        allowedOrigins.addAll(this.allowedOrigins);
         return allowedOrigins.toArray(new String[0]);
     }
 
